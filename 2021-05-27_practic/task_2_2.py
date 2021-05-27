b78cdaf59983a1b1b1b41a68dbd1bef410c586da
Andrey Smakotin <slamfromm@gmail.com>
2021-05-27 23:01:38 +0300

    Пользователь вводит номер месяца, вывести название месяца.

---
+++ 2021-05-27_practic/task_2_2.py
@@ -0,0 +1,27 @@
+# Пользователь вводит номер месяца, вывести название месяца.
+
+a = int(input('Введите номер месяца 1-12: '))
+if a == 1:
+    print('Январь')
+elif a == 2:
+    print('Февраль')
+elif a == 3:
+    print('Март')
+elif a == 4:
+    print('Апрель')
+elif a == 5:
+    print('Май')
+elif a == 6:
+    print('Июнь')
+elif a == 7:
+    print('Июль')
+elif a == 8:
+    print('Август')
+elif a == 9:
+    print('Сентябрь')
+elif a == 10:
+    print('Октябрь')
+elif a == 11:
+    print('Ноябрь')
+elif a == 12:
+    print('Декабрь')
